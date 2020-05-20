a201c085a67d921749dc7cfe30e676dc16d9bbb3
Ethan <ebeauvaisguibert@gmail.com>
2020-05-20 16:09:44 +0200

    LABY

--- LABYRINTHE/dedale.py
+++ LABYRINTHE/dedale.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import time
+
 
 
 incr = 2
@@ -24,31 +26,35 @@ def right():
     global posX
     global posY
     if (map[posY][posX + 1] == 1):
-        return
+        return False
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
     posX += 1
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
+        return 3
+    return True
 
 def left():
     global posX
     global posY
     if (map[posY][posX - 1] == 1):
-        return
+        return False
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
     posX -= 1
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
+        return 3
+    return True
 
 def up():
     global posY
     global posX
     if (map[posY - 1][posX] == 1):
-        return
+        return False
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
     posY -= 1
@@ -56,26 +62,57 @@ def up():
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
+        return 3
+    return True
 
 def down():
     global posY
     global posX
     if (map[posY + 1][posX] == 1):
-        return
+        return False
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
     posY += 1
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
+        return 3
+    return True
 
 
 
 dir = []
 
-def algo(value):
-    down()
+#L'alogorythme s'appuie sur la technique bien connue qui est de suivre le mur à sa gauche
+#( ça marche aussi à droite )
 
+#Index = 1 car "left()" est à cette position dans le labyrinthe
+index = 0
+
+def algo(value):
+    #LISTE DES FONCTIONS QUE L'ON TESTERA AVEC LE eval()
+    functionToTest = ["up()" ,"left()", "down()", "right()"]
+
+    #Globalisation de la variable index qui sera l'index de la fonction à tester dans
+    #la liste functionToTest
+    global index
+
+
+    #Si de le mouvement de la fonction d'index "index" dans la liste "functionToTest"
+    #est possible, cela veut dire que le mur n'est plus à droite
+    if eval(functionToTest[index]):
+        index += 1
+
+        #Si l'index = 4, pour eviter de se retrouver en dehors de la liste, on la reprend
+        #du début.
+        if index == 4:
+            index = 0
+    else:
+        index -= 1
+
+        #Comme functionToTest comporte 4 élements, alors functionToTest[-5] = functionToTest[-1]
+        if index == -5:
+            index = -1
 
 
 
@@ -86,10 +123,10 @@ def setACaseXY(X, Y, color):
 
 
 master = Tk()
-canvas_width = 1000
+canvas_width = 1400
 canvas_height = 1000
 w = Canvas(master,
-           width=1000,
+           width=1400,
            height=1000)
 
 
@@ -120,9 +157,11 @@ w.pack()
 setACaseXY(47, 21, 'green')
 map[21][47] = 2
 
+lastMoov = ""
 
-for value in range(0, 1000):
+for value in range(0, 2000):
     master.after(value * 100, algo, value)
+
 
 checkered(w,10)
 
